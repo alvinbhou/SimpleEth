baf46b9c4709be589acdbc08935b085e6c79ad96
Alvin <cryolite@ntu.im>
2018-05-23 00:11:36 +0800

    add https server support

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var express = require('express');
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const config = require('./config.js')
 var app = express();
-var path = require('path');
-
 app.use(express.static(__dirname)); // Current directory is root
-
-app.listen(8000);
-console.log('Listening on port 8000');
+if (config.credential) {
+    let options = {
+        key: fs.readFileSync(config.credential.key),
+        cert: fs.readFileSync(config.credential.cert),
+        ca: fs.readFileSync(config.credential.ca),
+    }
+    https.createServer(options, app).listen(config.port, () => {
+        console.log(`server is listening on ${config.port} port...`);        
+    });
+}
+else {
+    app.listen(config.port, () => {
+        console.log(`server is listening on ${config.port} port...`);
+    });
+}
